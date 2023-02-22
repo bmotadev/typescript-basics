bbf8715b60dc79f2a774a4dea36b06d67c0f1190
Bruno Mota <bmotadev@gmail.com>
2023-02-22 17:13:54 -0300

    :sparkles: feat: functions em typescript

---
+++ aula_009_functions/dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+function exibirMensagem() {
+    console.log('Olá, seja bem-vindo.');
+}
+exibirMensagem(); // invoca a função
---
+++ aula_009_functions/src/index.ts
@@ -0,0 +1,5 @@
+function exibirMensagem() { //usar camelCase para nome de função
+    console.log('Olá, seja bem-vindo.');
+}
+
+exibirMensagem(); // invoca a função
