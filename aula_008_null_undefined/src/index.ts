d52d4dab076517ac891b9989fd9ff0ab62d5467d
Bruno Mota <bmotadev@gmail.com>
2023-02-22 11:49:45 -0300

    :sparkles: feat: null e undefined em typescript

---
+++ aula_008_null_undefined/src/index.ts
@@ -0,0 +1,9 @@
+//quando declaro uma variavel sem atribuir valor, é undefined
+//quando quero dizer que a variavel não possui valor, utiliza-se o null
+let nome = null; 
+
+console.log(nome);
+
+if(nome == undefined) {
+    console.log('É nulo');
+}
