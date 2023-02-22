dbd95cab9c381cb56629243068bf7a3c5bc08504
Bruno Mota <bmotadev@gmail.com>
2023-02-22 11:20:41 -0300

    :sparkles: feat: arrays em typescript

---
+++ aula_005_arrays/src/index.js
@@ -0,0 +1,11 @@
+"use strict";
+let notas = []; //array do tipo number
+let paises;
+let cidades = [
+    'São Paulo',
+    'Minas Gerais',
+    'Divinópolis'
+];
+notas.push(10);
+console.log(notas[0]);
+console.log(cidades.length);
---
+++ aula_005_arrays/src/index.ts
@@ -0,0 +1,15 @@
+let notas : number[] = []; //array do tipo number
+let paises : Array<string>;
+
+let cidades = [
+    'São Paulo',
+    'Minas Gerais',
+    'Divinópolis'
+];
+
+
+notas.push(10);
+
+console.log(notas[0]);
+
+console.log(cidades.length);
