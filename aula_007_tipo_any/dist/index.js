9cfefd09d93947ede944d9f68147bfa8027fa3af
Bruno Mota <bmotadev@gmail.com>
2023-02-22 11:43:16 -0300

    :sparkles: feat: typo any em typescript

---
+++ aula_007_tipo_any/dist/index.js
@@ -0,0 +1,11 @@
+"use strict";
+let nome; // recebe valor de qualquer tipo
+nome = 'Bruno';
+nome = 1;
+nome = {
+    idade: 18
+};
+nome = () => {
+    console.log('Olá.');
+};
+nome();
---
+++ aula_007_tipo_any/src/index.ts
@@ -0,0 +1,15 @@
+let nome : any; // recebe valor de qualquer tipo
+
+nome = 'Bruno';
+
+nome = 1;
+
+nome = {
+    idade: 18
+};
+
+nome = () => {
+    console.log('Olá.');
+};
+
+nome();
