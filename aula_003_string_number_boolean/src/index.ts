04bee4df578eea268b718426c18179454b92dae5
Bruno Mota <bmotadev@gmail.com>
2023-02-22 11:05:51 -0300

    :sparkles: feat: string number boolean

--- aula_003_string_number_boolean/src/index.ts
+++ aula_003_string_number_boolean/src/index.ts
@@ -1,1 +1,20 @@
-console.log('teste');
+//string
+let nome = 'Bruno';
+let email = "bruno@email.com";
+console.log(typeof nome);
+
+//number
+let idade = 29;
+let temperatura = 36.7;
+console.log(typeof idade);
+console.log(typeof temperatura);
+
+//boolean
+let verdadeiro = true;
+let falso = false;
+console.log(typeof verdadeiro);
+console.log(typeof falso);
+
+console.log('A' + 'B');
+console.log('A' + 'B' + 3);
+console.log(4 + 3);
