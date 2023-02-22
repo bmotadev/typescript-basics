7bda00a13d5aba286674a3eb6de397fd2039c42e
Bruno Mota <bmotadev@gmail.com>
2023-02-22 11:37:36 -0300

    :sparkles: feat: objetos em typescript

---
+++ aula_006_objetos/dist/index.js
@@ -0,0 +1,26 @@
+"use strict";
+let cliente = {
+    nome: 'Bruno',
+    email: 'bruno@email.com',
+    saldo: 100,
+    ativo: true
+};
+cliente = {
+    nome: 'Jessica',
+    email: 'jessica@email.com',
+    saldo: 200,
+    ativo: false
+};
+let produto = {
+    codigo: 1,
+    descricao: 'Arroz',
+    categoria: {
+        codigo: 10,
+        descricao: 'Cereais'
+    }
+};
+let aluno;
+aluno = {
+    matricula: '001',
+    nome: 'João'
+};
---
+++ aula_006_objetos/src/index.ts
@@ -0,0 +1,32 @@
+let cliente = {
+    nome: 'Bruno',
+    email: 'bruno@email.com',
+    saldo: 100,
+    ativo: true
+}
+
+cliente = {
+    nome: 'Jessica',
+    email: 'jessica@email.com',
+    saldo: 200,
+    ativo: false
+}
+
+let produto = {
+    codigo: 1,
+    descricao: 'Arroz',
+    categoria: { //subpropriedade
+        codigo: 10,
+        descricao: 'Cereais'
+    }
+}
+
+let aluno : {
+    matricula: string,
+    nome: string
+}
+
+aluno = {
+    matricula: '001',
+    nome: 'João'
+}
