c6a315cdc0e771ea9b73d8c14ca4da49e10fea77
Bruno Mota <bmotadev@gmail.com>
2023-02-22 11:13:16 -0300

    :sparkles: feat: type inference e annotations

---
+++ aula_004_type_inference_annotations/dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+let nome = 'Bruno';
+let idade = 9; //type annotations
+idade = 10;
+idade = true;
+console.log(idade);
+let curso;
+curso = 1;
+curso = 'TESTE';
---
+++ aula_004_type_inference_annotations/src/index.ts
@@ -0,0 +1,15 @@
+let nome = 'Bruno';
+
+let idade:number = 9; //type annotations
+
+idade = 10;
+
+idade = true;
+
+console.log(idade);
+
+let curso;
+
+curso = 1;
+
+curso = 'TESTE';
