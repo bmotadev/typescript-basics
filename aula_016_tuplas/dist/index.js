d64f974f16841b04c48cdb1e59e8faee0b610141
Bruno Mota <bmotadev@gmail.com>
2023-02-23 20:53:20 -0300

    :sparkles: feat: tuplas typescript

---
+++ aula_016_tuplas/dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+// tuplas representam um conjunto de informações agrupadas
+let coodernada;
+coodernada = [10, 20];
+console.log(coodernada[0]);
+console.log(coodernada[1]);
+let [x, y] = coodernada; //descontruindo a tupla, pegando duas variaveis individuais
+console.log(x, y);
---
+++ aula_016_tuplas/src/index.ts
@@ -0,0 +1,12 @@
+// tuplas representam um conjunto de informações agrupadas
+
+let coodernada : [ number, number];
+
+coodernada = [10,20];
+
+console.log(coodernada[0]);
+console.log(coodernada[1]);
+
+let [x, y] = coodernada; //descontruindo a tupla, pegando duas variaveis individuais
+
+console.log(x, y);
